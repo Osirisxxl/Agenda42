d183a45b14ceeb1532ad79e33a545be8b6c7b69f
Osirisxxl <osirisxxxl@gmail.com>
2013-05-03 16:58:07 +0200

    Basic JQM structure for two pages + Grid on first page + JQuery Ajax CORS request enabled.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,62 @@
+(function () {
+      "use strict";
+
+    $(document).ready(function(){
+        //Init selected values;
+        document.agenda42 = {};
+        document.agenda42.selections = {
+            date : new Date()
+        };
+        document.agenda42.webService = {
+            baseURI : "http://10.33.33.4:8080/agenda/resources/agenda/",
+            ownerID : "testJeremie"
+        };
+
+
+        //Init Calendar buttons
+        var $myCalendarButtons = $( "#calendar" ).find( "button" );
+        $myCalendarButtons.on("click", function(){
+            $.mobile.changePage( "#day", { transition: "slide" });
+            document.agenda42.selections.date.setDate( $(this).html() );
+            $( "#day" ).find( "div:first" ).find( "h1" ).html( document.agenda42.selections.date.toLocaleDateString());
+
+            $.support.cors = true;
+            var date = document.agenda42.selections.date;
+            $.ajax({
+                // the URL for the request
+                url: document.agenda42.webService.baseURI + "events/" +
+                    document.agenda42.webService.ownerID + "/" +date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate(),
+
+                // whether this is a POST or GET request
+                type: "GET",
+
+                //Enable CORS
+                crossDomain : true,
+
+                // the type of data we expect back
+                dataType : "json",
+                contentType: "application/json",
+
+                // code to run if the request succeeds;
+                // the response is passed to the function
+                success: function( json ) {
+                    $( "#dayResults" ).html( json.toString() );
+                },
+
+                // code to run if the request fails; the raw request and
+                // status codes are passed to the function
+                error: function( xhr, status ) {
+                    alert( "Sorry, there was a problem!" );
+                },
+
+                // code to run regardless of success or failure
+                complete: function( xhr, status ) {
+                }
+            });
+        });
+
+
+
+    });
+ })();
 
