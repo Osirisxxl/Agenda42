85d278c4dea6e1fd9b0c0a2f83df1a478de2d988
Osirisxxl <osirisxxxl@gmail.com>
2013-05-14 17:55:38 +0200

    Add support for creating and editing events

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@
 
         //Init Calendar buttons
         var $myCalendarButtons = $( "#calendar" ).find( "a" );
-        $myCalendarButtons.on("vclick", function(){
+        $myCalendarButtons.on("tap", function(){
             //clears events display
             $( "#dayResults").empty();
             //Navigation
@@ -52,18 +52,25 @@
                     var $results = $( "#dayResults" );
 
                     for (var i = 0; i < results.length; i++ ) {
-                        $results.append( "<div data-role='collapsible'>" +
-                                            "<h3>"+results[i].name + "</h3>" +
-                                            "<p>" + results[i].description + "</p>" +
-                                        "</div> ");
+                        var $newEventCollapsible = $( "<div data-role='collapsible' id='event" + i + "'>" +
+                            "<h3>"+results[i].name + "</h3>" +
+                            "<p>" + results[i].description + "</p>" +
+                            "</div> " );
+                        $newEventCollapsible.bind('expand',function(){
+
+                            document.agenda42.selections.selectedEvent = document.agenda42.selections.results[parseInt(this.id.substring(5))];
+                        });
+
+                        $results.append( $newEventCollapsible );
                     }
                     $results.collapsibleset( "refresh" );
+                    document.agenda42.selections.results = results;
                 },
 
                 // code to run if the request fails; the raw request and
                 // status codes are passed to the function
                 error: function( xhr, status ) {
-                    alert( "Sorry, there was a problem!" );
+                    alert( "Server appears to be unreachable.\n Check your connection" );
                 },
 
                 // code to run regardless of success or failure
@@ -71,6 +78,115 @@
                 }
             });
         });
+
+        //Fill edit event page
+        $( "#eventEdit").on("pagebeforeshow",function(){
+            var selectedEvent = document.agenda42.selections.selectedEvent;
+
+            if(selectedEvent === undefined) {
+                $( "#eventEditHeader").html( "New event" );
+                //Reset the values to default
+                $("#eventName").val("");
+                $("#eventBeginDate").val((new Date()).toJSON().slice(0,10));
+                var $eventDuration = $("#eventDuration");
+                $eventDuration.val(60);
+                try{$eventDuration.slider("refresh")}catch(e){}
+                $("#eventBeginTime").val((new Date()).toLocaleTimeString());
+                var $eventAlarm = $("#eventAlarm");
+                try{$eventAlarm.val('off');}catch (e){}
+                $( "#eventDescription").val("");
+                $( "#eventPlace").val("");
+            }
+            else{
+                // Display correct header
+                $( "#eventEditHeader").html( "Edit event" );
+
+                //Display event info
+                $( "#eventName").val(selectedEvent.name);
+                var date = new Date(selectedEvent.beginDate);
+                $( "#eventBeginDate").val(date.toJSON().slice(0,10));
+
+                var $eventDuration = $("#eventDuration");
+                $eventDuration.val(selectedEvent.duration);
+                try{
+                    $eventDuration.slider("refresh");
+                }catch (e){}
+
+                $( "#eventBeginTime").val(date.toLocaleTimeString());
+
+                var $eventAlarm = $("#eventAlarm");
+                if(selectedEvent.alert){
+                    $eventAlarm.val('on');
+                }else{
+                    $eventAlarm.val('off');
+                }
+                try{$eventAlarm.slider("refresh")}catch (e){}
+                $( "#eventDescription").val(selectedEvent.description);
+                $( "#eventPlace").val(selectedEvent.place);
+            }
+        });
+        //Init new event action click
+        $( "#newEventAction" ).on("tap", function(){
+            //Removing previous event selection
+            document.agenda42.selections.selectedEvent = undefined;
+            //collapse events
+            $("#dayResults").find('div[data-role="collapsible"]')
+            .each(function(){$(this).trigger("collapse");});
+
+        });
+
+        //Init the saveEvent click
+        $( "#saveEvent").on("tap", function(){
+            $.mobile.loading( "show" );
+            //Build data to send
+            var data = document.agenda42.selections.selectedEvent || {};
+            data.name = $( "#eventName" ).val();
+            data.duration = $( "#eventDuration" ).val();
+            data.alert = $( "#eventAlarm" ).val()==="on";
+            data.place = $( "#eventPlace" ).val();
+            data.description = $( "#eventDescription" ).val();
+            data.ownerID = document.agenda42.webService.ownerID;
+            var date = new Date(
+                $( "#eventBeginDate" ).val() + " "
+                + $( "#eventBeginTime" ).val()
+            );
+            data.beginDate = date.getFullYear()+"-"+(date.getMonth()+1) +"-"+date.getDate()+"-"
+                + date.getHours()+"-"+date.getMinutes();
+            $.ajax({
+                // the URL for the request
+                url: document.agenda42.webService.baseURI + ((document.agenda42.selections.selectedEvent ===undefined)? "createEvent":"modifyEvent"),
+
+                // whether this is a POST or GET request
+                type: (document.agenda42.selections.selectedEvent ===undefined)? "POST":"PUT",
+                data: JSON.stringify(data),
+
+                //Enable CORS
+                crossDomain : true,
+
+                // the type of data we expect back
+                dataType : "json",
+                contentType: "application/json",
+
+                // code to run if the request succeeds;
+                // the response is passed to the function
+                success: function( results ) {
+
+                },
+
+                // code to run if the request fails; the raw request and
+                // status codes are passed to the function
+                error: function( xhr, status ) {
+                    alert( "The server appears to be unreachable.\n Check your connection" + status.toString());
+
+                },
+                // code to run regardless of success or failure
+                complete: function() {
+                    $.mobile.loading( "hide" );
+                    $.mobile.changePage( "#month", { transition: "slidedown" });
+                }
+            });
+        });
+
     });
 
  })();
