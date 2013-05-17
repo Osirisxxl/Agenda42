402950b98c801ef15671568a298cc6a502b825ae
Osirisxxl <osirisxxxl@gmail.com>
2013-05-17 11:13:21 +0200

    Added update of number of days in a month

--- js/main.js
+++ js/main.js
@@ -1,20 +1,43 @@
 (function () {
       "use strict";
+    //Creating a global object for utilities and user selections
     window.agenda42 = {};
+    agenda42.utils= { month : [{name:"Jan", dayNumber : 31},
+        {name:"Feb", dayNumber : 28}, {name:"Mar", dayNumber : 31},
+        {name:"Apr", dayNumber : 30}, {name:"May", dayNumber : 31},
+        {name:"Jun", dayNumber : 30}, {name:"Jul", dayNumber : 31},
+        {name:"Aug", dayNumber : 31}, {name:"Sep", dayNumber : 30},
+        {name:"Oct", dayNumber : 31}, {name:"Nov", dayNumber : 30},
+        {name:"Dec", dayNumber : 31}],
+        hideExtraDayButtons : function() {
+            var buttons = $( "#calendar").find("a");
+            for(var i = 0; i <= 30  ; i++){
+                if(i < agenda42.utils.month[agenda42.selections.date.getMonth()].dayNumber){
+                    buttons.eq(i).css("display", "block");
+                } else {
+                    buttons.eq(i).css("display", "none");
+                }
+            }
 
+        }
+    };
+    agenda42.webService = {
+        baseURI : "http://10.33.33.4:8080/agenda/resources/agenda/",
+        ownerID : "testJeremie"
+    };
+
+    //DOM is ready
     $(document).ready(function(){
-        /*//Init fastclick
-        $(function() {
-            FastClick.attach(document.body);
-        });*/
-        //Init selected values;
+        //Init selected values and month view
         agenda42.selections = {
             date : new Date()
         };
-        agenda42.webService = {
-            baseURI : "http://10.33.33.4:8080/agenda/resources/agenda/",
-            ownerID : "testJeremie"
-        };
+        //Display correct numbers of buttons and month name
+        $( "#monthDisplay").text(agenda42.utils.month[agenda42.selections.date.getMonth()].name
+            + " " +agenda42.selections.date.getFullYear());
+        agenda42.utils.hideExtraDayButtons();
+
+
 
 
         //Init Calendar buttons
@@ -24,7 +47,6 @@
             $.mobile.changePage( "#day", { transition: "slide" });
             //Sets selected date
             agenda42.selections.date.setDate( $(this).text() );
-
         });
 
         //Fill day page
@@ -64,7 +86,6 @@
                             "<p>" + results[i].description + "</p>" +
                             "</div> " );
                         $newEventCollapsible.bind('expand',function(){
-
                             agenda42.selections.selectedEvent = agenda42.selections.results[parseInt(this.id.substring(5))];
                         });
 
@@ -91,24 +112,27 @@
         //Fill edit event page
         $( "#eventEdit").on("pagebeforeshow",function(){
             var selectedEvent = agenda42.selections.selectedEvent;
-
+            // If no event has been selected in the first place => new event
             if(selectedEvent === undefined) {
                 $( "#eventEditHeader").html( "New event" );
                 //Reset the values to default
                 $("#eventName").val("");
-                $("#eventBeginDate").val((new Date()).toJSON().slice(0,10));
+                var date = new Date();
+                $("#eventBeginDate").val(date.toJSON().slice(0,10));
                 var $eventDuration = $("#eventDuration");
                 $eventDuration.val(60);
                 try{$eventDuration.slider("refresh")}catch(e){}
-                $("#eventBeginTime").val((new Date()).toLocaleTimeString());
+                $("#eventBeginTime").val(date.getHours()+date.getTimezoneOffset()/60 + ":" + date.getMinutes());
                 var $eventAlarm = $("#eventAlarm");
                 try{$eventAlarm.val('off');}catch (e){}
                 $( "#eventDescription").val("");
                 $( "#eventPlace").val("");
             }
             else{
+                // Set the event data we're editing
                 // Display correct header
                 $( "#eventEditHeader").html( "Edit event" );
+                //Parsing date(to ensure support across all browsers
                 var s = agenda42.selections.selectedEvent.beginDate;
                 var a = s.split(/[^0-9]/);
                 var date = new Date (a[0],a[1]-1,a[2],a[3],a[4],a[5]) || new Date();
@@ -140,10 +164,6 @@
         $( "#newEventAction" ).on("tap", function(){
             //Removing previous event selection
             agenda42.selections.selectedEvent = undefined;
-            //collapse events
-            $("#dayResults").find('div[data-role="collapsible"]')
-            .each(function(){$(this).trigger("collapse");});
-
         });
 
         //Init the saveEvent click
@@ -181,14 +201,11 @@
                 // code to run if the request succeeds;
                 // the response is passed to the function
                 success: function( results ) {
-
                 },
-
                 // code to run if the request fails; the raw request and
                 // status codes are passed to the function
                 error: function( xhr, status ) {
                     alert( "The server appears to be unreachable.\n Check your connection");
-
                 },
                 // code to run regardless of success or failure
                 complete: function() {
@@ -209,28 +226,21 @@
                 $.ajax({
                     // the URL for the request
                     url: agenda42.webService.baseURI + "deleteEvent",
-
                     //Type of request
                     type: "DELETE",
                     data: JSON.stringify(agenda42.selections.selectedEvent),
-
                     //Enable CORS
                     crossDomain : true,
-
                     // the type of data we expect back
                     contentType: "application/json",
-
                     // code to run if the request succeeds;
                     // the response is passed to the function
                     success: function( results ) {
-
                     },
-
                     // code to run if the request fails; the raw request and
                     // status codes are passed to the function
                     error: function( xhr, status ) {
                         alert( "The server appears to be unreachable.\n Check your connection");
-
                     },
                     // code to run regardless of success or failure
                     complete: function() {
@@ -239,6 +249,23 @@
                     }
                 });
             }
+        });
+
+        // Init next month button
+        $( "#nextMonth").on("tap", function(){
+            var date = agenda42.selections.date;
+            date.setMonth(date.getMonth() + 1);
+            $( "#monthDisplay").text(agenda42.utils.month[date.getMonth()].name + " " +date.getFullYear());
+            //determines number of buttons and wether to show or hide buttons
+            agenda42.utils.hideExtraDayButtons();
+        });
+        // Init prev month button
+        $( "#prevMonth").on("tap", function(){
+            var date = agenda42.selections.date;
+            date.setMonth(date.getMonth() - 1);
+            $( "#monthDisplay").text(agenda42.utils.month[date.getMonth()].name + " " +date.getFullYear());
+            //determines number of buttons and wether to show or hide buttons
+            agenda42.utils.hideExtraDayButtons();
         });
     });
 
