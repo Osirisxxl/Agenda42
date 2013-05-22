fd21698bc3b0261c0cc69ddc68edd53bd47d090f
Osirisxxl <osirisxxxl@gmail.com>
2013-05-22 16:00:28 +0200

    Test ...

--- js/main.js
+++ js/main.js
@@ -123,6 +123,7 @@
                 var $eventDuration = $("#eventDuration");
                 $eventDuration.val(60);
                 try{$eventDuration.slider("refresh")}catch(e){}
+                alert(date.toLocaleTimeString().slice(0,5));
                 $("#eventBeginTime").val(date.toLocaleTimeString().slice(0,5));
                 var $eventAlarm = $("#eventAlarm");
                 try{$eventAlarm.val('off');}catch (e){}
